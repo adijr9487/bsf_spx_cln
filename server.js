30f9b4c0306f9247bd74ef21031f42af8cb47409
Aditya <adijr9487@gmail.com>
2023-08-12 11:52:39 +0530

    updated server.js file with auth and spacex routes

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const db = require("./config/db_config");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
 // Routers
-// const home = require("./routes/home");
-// const auth = require("./routes/auth");
-// const search = require("./routes/search");
+const auth = require("./routes/auth");
+const spacex = require("./routes/spacex");
 
 // PORT
 const PORT = process.env.PORT || 5000;
@@ -19,13 +19,18 @@ const app = express();
 //MiddleWares
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(cors({
+  origin: '*', // Replace with your frontend's origin
+  credentials: true,
+}));
+
 app.use("/public", express.static(path.resolve(__dirname, "./public")));
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // Routes
-// app.use("/", home);
-// app.use("/auth", auth);
-// app.use("/search", search);
+app.use("/api/auth", auth);
+app.use("/api/spacex", spacex);
 
 if (process.env.NODE_ENV === "production") {
   // Basename is client should be '/app'
@@ -38,8 +43,8 @@ if (process.env.NODE_ENV === "production") {
 db.connect(process.env.MONGO_URI)
 .then(() => {
 
-    //starting server
-    app.listen(PORT, () => {
+  //starting server
+  app.listen(PORT, () => {
     console.log("Server is up and running! PORT=", PORT);
-    });
+  });
 })
